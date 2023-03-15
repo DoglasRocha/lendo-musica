05570e381d8a78389dea7adb5155065f16510ebf
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2023-03-14 22:49:02 -0300

    start lyric screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import "./css/basic-setup.css";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import History from "./pages/History";
 import Found from "./pages/Found";
-import "./css/basic-setup.css";
+import Lyric from "./pages/Lyric";
 
 function App() {
   return (
     <>
-      <Found />
+      <Lyric />
     </>
   );
 }
---
+++ src/pages/Lyric.jsx
@@ -0,0 +1,27 @@
+import MainContainer from "../components/MainContainer";
+import Navbar from "../components/Navbar";
+import NavText from "../components/NavText";
+import Logo from "../components/Logo";
+import Title from "../components/Title";
+import Text from "../components/Text";
+import SearchButton from "../components/SearchButton";
+import LogoImg from "../img/Logo azul.svg";
+import "../css/lyric.css";
+
+export default function Lyric() {
+  return (
+    <MainContainer>
+      <Navbar>
+        <Logo src={LogoImg} alt="Logo do Lendo Música" />
+        <NavText text="Voltar para a busca" />
+      </Navbar>
+      <Title text="Rap God" />
+      <Text
+        text="
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, doloremque maxime? Sequi excepturi iste quis reiciendis quos atque? Ea ipsam voluptates veniam hic sapiente earum provident nesciunt a in sed!"
+      />
+      <Text text="Curtiu? Busque mais letras" />
+      <SearchButton text="Nova busca"></SearchButton>
+    </MainContainer>
+  );
+}
